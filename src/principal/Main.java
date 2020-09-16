0efbed8f7c55edb91c75d3beacf37a48f7639443
EdDenb <edelvamdenberg@yahoo.com.br>
2020-09-15 21:28:09 -0300

    composicao DataHora classes Data, Hora e DataHora

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,10 +1,21 @@
 package principal;
 
+
+import modelo.DataHora;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("impressao teste");
+		System.out.println("Boa noite");
+		DataHora dtH1,dtH2;
+		dtH1 = new DataHora(12,12,1950);
+		dtH2 = new DataHora(12,12,1950,6,15,0);
+		System.out.println("Data 1: ");
+		System.out.println(dtH1);
+		System.out.println("Data 2: ");
+		System.out.println(dtH2);
+		}
 
 	}
 
-}
+
